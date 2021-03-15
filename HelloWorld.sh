943c2913d1aef276f863a0d9fc4ca7fce60688be
svasandani <vasandanishailesh@gmail.com>
2021-03-15 12:03:43 -0400

    Updating shell script

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,2 +1,5 @@
-javac HelloWorld.java
-java HelloWorld
+#!/bin/bash
+echo "Compiling"
+srun --mem=1GB --time=00:01:00 --cpus-per-task=1 javac HelloWorld.java
+echo "Running"
+srun --mem=1GB --time=00:15:00 --cpus-per-task=1 java HelloWorld
