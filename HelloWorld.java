7f6d9c31b01b2b5cf8f7327d704cac3c9389b584
svasandani <vasandanishailesh@gmail.com>
2021-03-15 12:00:42 -0400

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        Scanner input = new Scanner(System.in);
+
+        String token = input.nextLine();
+
+        System.out.println(token);
+    }
+}
