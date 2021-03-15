f406f4919296931a9dc9bba0328efdcab3003316
svasandani <vasandanishailesh@gmail.com>
2021-03-15 12:29:03 -0400

    Remigrate to Eclipse

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -2,7 +2,5 @@
 
 module load jdk/1.8.0_271
 
-echo "Compiling"
-srun --mem=1GB --time=00:01:00 --cpus-per-task=1 javac HelloWorld.java
-echo "Running"
-srun --mem=1GB --time=00:15:00 --cpus-per-task=1 java HelloWorld
+echo "Running JAR file"
+srun --mem=1GB --time=00:01:00 --cpus-per-task=1 java -jar output/HelloWorld.jar
