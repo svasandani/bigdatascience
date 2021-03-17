45175ddd3acf5f78b8c176066147c9d8888fbbc1
svasandani <vasandanishailesh@gmail.com>
2021-03-17 18:07:14 -0400

    Convert NgramAnalysis to OOP

--- NgramAnalysis.java
+++ NgramAnalysis.java
@@ -1,62 +1,59 @@
 import java.io.*;
 import java.util.*;
-import java.util.Map.*;
-import java.util.stream.*;
 
 public class NgramAnalysis {
 	
-	final static int TEXT_COLUMN = 5;
-	final static String[] EXCLUDE = {"\"", " ", "", "i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","should","now"};
-
+	private int TEXT_COLUMN = 5;
+	private String[] EXCLUDE = {"\"", " ", "", "i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","should","now"};
+	private String filename;
+	private ArrayList<String[]> table;
+	
+	public NgramAnalysis(String filename) throws FileNotFoundException {
+		this.filename = filename;
+		
+		CSVParser c = new CSVParser(filename);
+		this.table = c.parse();
+	}
+	
+	public NgramAnalysis(ArrayList<String[]> table) {
+		this.table = table;
+	}
+	
+	public void setTextColumn(int textColumn) {
+		this.TEXT_COLUMN = textColumn;
+	}
+	
 	public static void main(String[] args) throws FileNotFoundException {
-		CSVParser c = new CSVParser("sentiment140.csv");
+		CSVParser c = new CSVParser("sentiment500.csv");
 		ArrayList<String[]> table = c.parse();
 //		c.preview(2);
 		
-		Map<String, Integer> oneGrams = Util.trimMap(Util.sortMap(collectNgrams(table, 1)), 20);
-		Map<String, Integer> twoGrams = Util.trimMap(Util.sortMap(collectNgrams(table, 2)), 20);
-		Map<String, Integer> threeGrams = Util.trimMap(Util.sortMap(collectNgrams(table, 3)), 20);
-		Map<String, Integer> fourGrams = Util.trimMap(Util.sortMap(collectNgrams(table, 4)), 20);
+		NgramAnalysis me = new NgramAnalysis(table);
+		
+		Map<String, Integer>[] nGrams = me.analyzeRange(1, 4, 20);
 
-		System.out.println(oneGrams.toString());
-		System.out.println(twoGrams.toString());
-		System.out.println(threeGrams.toString());
-		System.out.println(fourGrams.toString());
+		for (Map<String, Integer> nGram : nGrams) {
+			System.out.println(nGram);
+		}
 	}
 	
-	public static String[] removeStopwords(String[] textArray) {
-		int excludedLength = 0;
-		for (String str : textArray) {
-			boolean excludeString = false;
-			
-			for (int k = 0; k < EXCLUDE.length; k++) {
-				if (str.toLowerCase().equals(EXCLUDE[k])) excludeString = true;
-			}
-			
-			if (!excludeString) {
-				excludedLength++;
-			}
+	public Map<String, Integer>[] analyzeRange(int start, int end, int limit) {
+		if (end <= start) return null;
+		
+		Map<String, Integer>[] range = new Map[end - start + 1];
+		
+		for (int i = start; i <= end; i++) {
+			range[i - start] = analyzeTop(i, limit);
 		}
 		
-		String[] excludedTextArray = new String[excludedLength];
-		int excludedIndex = 0;
-		for (String str : textArray) {
-			boolean excludeString = false;
-					
-			for (int k = 0; k < EXCLUDE.length; k++) {
-				if (str.toLowerCase().equals(EXCLUDE[k])) excludeString = true;
-			}
-			
-			if (!excludeString) {
-				excludedTextArray[excludedIndex] = str;
-				excludedIndex++;
-			}
-		}
-		
-		return excludedTextArray;
+		return range;
 	}
 	
-	public static LinkedHashMap<String, Integer> collectNgrams(ArrayList<String[]> table, int n) {
+	public Map<String, Integer> analyzeTop(int n, int limit) {
+		return Util.trimMap(Util.sortMap(collectNgrams(table, n)), limit);
+	}
+	
+	public LinkedHashMap<String, Integer> collectNgrams(ArrayList<String[]> table, int n) {
 		System.out.printf("Collecting %s-grams...\n", n);
 		
 		LinkedHashMap<String, Integer> nGrams = new LinkedHashMap<String, Integer>();
@@ -67,13 +64,15 @@ public class NgramAnalysis {
 			String[] textArray = s[TEXT_COLUMN].split(" ");
 			
 			// remove quotation marks from CSV
-			if (textArray[0].length() > 0) 
+			if (textArray[0].length() > 0 && textArray[0].charAt(0) == '\"') 
 				textArray[0] = textArray[0].substring(1);
-			if (textArray[textArray.length - 1].length() > 0) 
+			if (textArray[textArray.length - 1].length() > 0 
+			    && textArray[textArray.length - 1]
+					 .charAt(textArray[textArray.length - 1].length() - 1) == '\"') 
 				textArray[textArray.length - 1] = textArray[textArray.length - 1]
 					.substring(0, textArray[textArray.length - 1].length() - 1);
 			
-			String[] excludedTextArray = removeStopwords(textArray);
+			String[] excludedTextArray = Util.removeStopwords(textArray, EXCLUDE);
 			
 			if (excludedTextArray.length < n) continue;
 			
