2f7ead48d3198605f5be84a610e97dcbeb7c2578
svasandani <vasandanishailesh@gmail.com>
2021-03-17 18:53:37 -0400

    Update JARs

--- KeywordExtraction.sh
+++ KeywordExtraction.sh
@@ -2,6 +2,9 @@
 
 module load jdk/1.8.0_271
 
+for file in `find /scratch/sav339/keyword_extraction/stanford-corenlp-4.2.0/ -name "*.jar"`; do export
+CLASSPATH="$CLASSPATH:`realpath $file`"; done
+
 echo "Compiling"
 srun --mem=8GB --time=00:20:00 --cpus-per-task=1 javac RuleBasedKeywordExtraction.java
 echo "Running"
