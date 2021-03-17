548ab166b513e38b4aac91a311ace892fcbd0beb
svasandani <vasandanishailesh@gmail.com>
2021-03-17 18:35:55 -0400

    Delete jar

--- KeywordExtraction.sh
+++ KeywordExtraction.sh
@@ -2,5 +2,7 @@
 
 module load jdk/1.8.0_271
 
-echo "Running JAR"
-srun --mem=8GB --time=00:20:00 --cpus-per-task=1 java -jar outpput/RuleBasedKeywordExtraction.jar
+echo "Compiling"
+srun --mem=8GB --time=00:20:00 --cpus-per-task=1 javac RuleBasedKeywordExtraction.java
+echo "Running"
+srun --mem=8GB --time=00:20:00 --cpus-per-task=1 java RuleBasedKeywordExtraction
