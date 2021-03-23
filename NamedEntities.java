0dde02001529a52f7dcedeb9a88a519ca7928bd9
svasandani <vasandanishailesh@gmail.com>
2021-03-22 23:20:13 -0400

    Oops

--- NamedEntities.java
+++ NamedEntities.java
@@ -13,7 +13,7 @@ public class NamedEntities {
 	final static String[] EXCLUDE = {"\"", " ", "", "i","me","my","myself","we","our","ours","ourselves","you","your","yours","yourself","yourselves","he","him","his","himself","she","her","hers","herself","it","its","itself","they","them","their","theirs","themselves","what","which","who","whom","this","that","these","those","am","is","are","was","were","be","been","being","have","has","had","having","do","does","did","doing","a","an","the","and","but","if","or","because","as","until","while","of","at","by","for","with","about","against","between","into","through","during","before","after","above","below","to","from","up","down","in","out","on","off","over","under","again","further","then","once","here","there","when","where","why","how","all","any","both","each","few","more","most","other","some","such","no","nor","not","only","own","same","so","than","too","very","s","t","can","will","just","don","should","now"};
 	
 	public static void main(String[] args) throws FileNotFoundException {
-		CSVParser c = new CSVParser("sentiment500.csv");
+		CSVParser c = new CSVParser("sentiment140.csv");
 		ArrayList<String[]> table = c.parse();
 //		c.preview(2);       
 		
